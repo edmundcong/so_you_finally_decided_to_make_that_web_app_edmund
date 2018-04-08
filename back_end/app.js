26f1464785f7f69d85578c8e1894e4f561c5256e
edmundcong1 <eddy9412@gmail.com>
2018-04-08 23:51:37 +1000

    brief architecturing into front end and back end and a simple tesitng route

---
+++ back_end/app.js
@@ -0,0 +1,26 @@
+// index.js
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+let index = require("./routes/index");
+
+// don't need this YET
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+let port = process.env.PORT || 8080;
+
+// routes for our API
+let router = express.Router();
+
+// home route
+router.get("/", index);
+
+// register our routes
+app.use("/api", router);
+
+// start server
+app.listen(port);
+console.log("operating on: " + port);
